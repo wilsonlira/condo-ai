9c11c51ad26788947a42e6adc869a1d5d2db970b
wilsonlira <155825837+wilsonlira@users.noreply.github.com>
2025-02-21 22:40:55 +0000

    Sua mensagem de commit aqui

--- condo-ai-backend/database.py
+++ condo-ai-backend/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://admin:admin@localhost:5432/condo_ai"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
